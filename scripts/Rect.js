c2c5453467d659dbc12acba4e81568efabacc6f6
Andrew Savelev <savelevcorr@yandex.ru>
2016-10-19 17:16:38 +0300

    animation ball

--- scripts/Rect.js
+++ scripts/Rect.js
@@ -3,6 +3,6 @@ import Vec from './Vec';
 export default class Rect {
 	constructor(w, h) {
 		this.pos = new Vec();
-		this.sixe = new Vec(w, h);
+		this.size = new Vec(w, h);
 	}
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,47 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './App';
+import Rect from './Rect';
+import Ball from './Ball';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+// Constants
+const CANVAS = document.getElementById('pong');
+const CONTEXT = CANVAS.getContext('2d');
+const BALL = new Ball();
+
+let lastTime = 0;
+
+
+BALL.pos.x = 100;
+BALL.pos.y = 50;
+
+BALL.vel.x = 100;
+BALL.vel.y = 100;
+
+function callBack(ms) {
+	if (lastTime) {
+		update((ms - lastTime) / 1000);
+	}
+
+	lastTime = ms;
+
+	requestAnimationFrame(callBack)
+}
+
+function update(dt) {
+	BALL.pos.x += BALL.vel.x * dt;
+	BALL.pos.y += BALL.vel.y * dt;
+
+	if (BALL.pos.x < 0 || BALL.pos.x > CANVAS.width) {
+		BALL.vel.x = -BALL.vel.x;
+	}
+
+	if (BALL.pos.y < 0 || BALL.pos.y > CANVAS.height) {
+		BALL.vel.y = -BALL.vel.y;
+	}
+
+	CONTEXT.fillStyle = '#000';
+	CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
+
+	CONTEXT.fillStyle = '#fff';
+	CONTEXT.fillRect(BALL.pos.x, BALL.pos.y, BALL.size.x, BALL.size.y);
+}
+
+callBack();
