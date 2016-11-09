fd3fcd171ca6715869c957a363efd6a81ef6d357
Andrew Savelev <savelevcorr@yandex.ru>
2016-11-09 19:48:59 +0300

    refuctoring index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,7 @@
 import Rect from './Rect';
-import Ball from './Ball';
+import Pong from './Pong';
 
 // Constants
 const CANVAS = document.getElementById('pong');
-const CONTEXT = CANVAS.getContext('2d');
-const BALL = new Ball();
+const PONG = new Pong(CANVAS);
 
-let lastTime = 0;
-
-
-BALL.pos.x = 100;
-BALL.pos.y = 50;
-
-BALL.vel.x = 100;
-BALL.vel.y = 100;
-
-function callBack(ms) {
-	if (lastTime) {
-		update((ms - lastTime) / 1000);
-	}
-
-	lastTime = ms;
-
-	requestAnimationFrame(callBack)
-}
-
-function update(dt) {
-	BALL.pos.x += BALL.vel.x * dt;
-	BALL.pos.y += BALL.vel.y * dt;
-
-	if (BALL.left < 0 || BALL.right > CANVAS.width) {
-		BALL.vel.x = -BALL.vel.x;
-	}
-
-	if (BALL.top < 0 || BALL.bottom > CANVAS.height) {
-		BALL.vel.y = -BALL.vel.y;
-	}
-
-	CONTEXT.fillStyle = '#000';
-	CONTEXT.fillRect(0, 0, CANVAS.width, CANVAS.height);
-
-	CONTEXT.fillStyle = '#fff';
-	CONTEXT.fillRect(BALL.pos.x, BALL.pos.y, BALL.size.x, BALL.size.y);
-}
-
-callBack();
